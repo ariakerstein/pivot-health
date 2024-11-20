a5c0a2307fef31b2730f9f0325a7c73af5ae9925
ariakerstein <254822-ariakerstein@users.noreply.replit.com>
2024-11-20 20:34:36 +0000

    Restored to '74638124ee4258237c65c7cdb76270f2beb77c0b'
    Replit-Restored-To:74638124ee4258237c65c7cdb76270f2beb77c0b

--- forms.py
+++ forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FloatField, IntegerField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, FloatField, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
